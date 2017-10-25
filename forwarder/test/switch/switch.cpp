9fa82edf901104934900a94fd96f58b66a0ece4c
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2017-10-25 20:35:06 +0900

    implement Layer 2 switching

--- forwarder/test/switch/switch.cpp
+++ forwarder/test/switch/switch.cpp
@@ -8,6 +8,7 @@
 #include "../../src/NetIf.hpp"
 #include "../../src/MacAddress.hpp"
 #include "../../src/Ethernet.hpp"
+#include "../../src/HashMap.hpp"
 
 #define IF_NUM 2
 
@@ -31,9 +32,14 @@ int main(void){
     pfds[1].fd = netif[1].pd;
     pfds[1].events = POLLIN|POLLERR;
     
+    //MacAddrTable
+    HashMap<MacAddress, NetIf *> mac_tbl(256);
     
     //receive buffer
     uint8_t buf[2048];
+    
+    //送信先
+    NetIf *outif;
     
     for(;;){
         switch(poll(pfds, inter_n, 10)){
@@ -55,18 +61,26 @@ int main(void){
                         
                         if(packet.getType() == ETHTYPE_UNKNOWN) continue;
                         
+                        mac_tbl.update(packet.getSrc(), &netif[i]);
+                        outif = mac_tbl.get(packet.getDst());
+                        
+                        //ブロードキャストか、mac_tblに登録ない場合は全ポートに送信
+                        if((packet.getDst().toLong() == 0xFFFFFFFFFFFF) || (outif == nullptr)){
+                           for(int j = 0; j < inter_n; j++){
+                                if(j == i) continue;
+                                netif[j].send(buf, s);
+                            }
+                           continue;
+                        }
+                        
+                        outif->send(buf, s);
+                        
                         /*
                         printf("[size]%d\n", s);
                         printf("[eth]type : %.4x\n", packet.getType());
                         printf("[src]%" PRIx64 "\n", packet.getSrc().toLong());
                         printf("[dst]%" PRIx64 "\n", packet.getDst().toLong());
                         */
-                        
-                        //全ポートに送信
-                        for(int j = 0; j < inter_n; j++){
-                            if(j == i) continue;
-                            netif[j].send(buf, s);
-                        }
                     }
                 }
             }
