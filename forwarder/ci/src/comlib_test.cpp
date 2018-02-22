97ee6c6adaee56b83a65fd796bb077bb2cbe1abe
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-02-22 23:55:32 +0900

    add : [ci] comlib test code. strncpy(), atoi()

--- forwarder/ci/src/comlib_test.cpp
+++ forwarder/ci/src/comlib_test.cpp
@@ -1,4 +1,5 @@
 #include <cppunit/extensions/HelperMacros.h>
+#include <cstring>
 #include "comlib.hpp"
 
 #ifdef FIXTURE_NAME
@@ -7,20 +8,24 @@
 
 #define FIXTURE_NAME ComlibTest
 
-using namespace comlib;
+namespace clib = comlib;
 
 class FIXTURE_NAME : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(FIXTURE_NAME);
+    CPPUNIT_TEST(test_strncpy);
     CPPUNIT_TEST(test_strlen);
     CPPUNIT_TEST(test_isdigit);
+    CPPUNIT_TEST(test_atoi);
     CPPUNIT_TEST_SUITE_END();
 
 public:
     void setUp();
     void tearDown();
 protected:
+    void test_strncpy();
     void test_strlen();
     void test_isdigit();
+    void test_atoi();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(FIXTURE_NAME);
@@ -28,12 +33,34 @@ CPPUNIT_TEST_SUITE_REGISTRATION(FIXTURE_NAME);
 void FIXTURE_NAME::setUp(){}
 void FIXTURE_NAME::tearDown(){}
 
+void FIXTURE_NAME::test_strncpy(){
+    char *str;
+    
+    //配列の動的確保時に()つけると0で初期化されるんですが、これってC++規格？
+    str =  new char[16]();
+    CPPUNIT_ASSERT_EQUAL(true, str == clib::strncpy(str, (char *)"abc", 3));
+    CPPUNIT_ASSERT_EQUAL(0, strcmp(str, "abc"));
+    delete[] str;
+    
+    str = new char[16]();
+    CPPUNIT_ASSERT_EQUAL(true, str == clib::strncpy(str, (char *)"abc", 2));
+    CPPUNIT_ASSERT_EQUAL(0, strcmp(str, "ab"));
+    delete[] str;
+}
+
 void FIXTURE_NAME::test_strlen(){
-    CPPUNIT_ASSERT_EQUAL(6, strlen((char *)"string"));
+    CPPUNIT_ASSERT_EQUAL(6, clib::strlen((char *)"string"));
 }
 
 void FIXTURE_NAME::test_isdigit(){
-    CPPUNIT_ASSERT_EQUAL(true, isdigit('1'));
-    CPPUNIT_ASSERT_EQUAL(true, isdigit('9'));
-    CPPUNIT_ASSERT_EQUAL(false, isdigit('a'));
+    CPPUNIT_ASSERT_EQUAL(true, clib::isdigit('1'));
+    CPPUNIT_ASSERT_EQUAL(true, clib::isdigit('9'));
+    CPPUNIT_ASSERT_EQUAL(false, clib::isdigit('a'));
 }
+
+void FIXTURE_NAME::test_atoi(){
+    CPPUNIT_ASSERT_EQUAL(clib::atoi((char *)"1234567890"), 1234567890);
+    CPPUNIT_ASSERT_EQUAL(clib::atoi((char *)"12345a67890"), 12345);
+    CPPUNIT_ASSERT_EQUAL(clib::atoi((char *)"a1234567890"), 0);
+}
+
