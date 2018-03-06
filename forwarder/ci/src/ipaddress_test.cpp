294762d67ab223454d972f268815f4ca4aaa576b
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-03-06 22:38:14 +0900

    add : [ci] test code for IPaddress static functions and copy constructor.

--- forwarder/ci/src/ipaddress_test.cpp
+++ forwarder/ci/src/ipaddress_test.cpp
@@ -11,13 +11,18 @@
 class FIXTURE_NAME : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(FIXTURE_NAME);
     CPPUNIT_TEST(test_ipaddress_core);
+    CPPUNIT_TEST(test_ipaddress_static);
     CPPUNIT_TEST_SUITE_END();
 
+private:
+    void copy_constructor_test(IPAddress target, uint32_t expect_int_addr);
+    
 public:
     void setUp();
     void tearDown();
 protected:
     void test_ipaddress_core();
+    void test_ipaddress_static();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(FIXTURE_NAME);
@@ -25,6 +30,10 @@ CPPUNIT_TEST_SUITE_REGISTRATION(FIXTURE_NAME);
 void FIXTURE_NAME::setUp() {}
 
 void FIXTURE_NAME::tearDown() {}
+
+void FIXTURE_NAME::copy_constructor_test(IPAddress target, uint32_t expect_int_addr){
+    CPPUNIT_ASSERT_EQUAL(expect_int_addr, target.touInt());
+}
 
 void FIXTURE_NAME::test_ipaddress_core(){
     IPAddress *addr1 = new IPAddress();
@@ -36,7 +45,7 @@ void FIXTURE_NAME::test_ipaddress_core(){
     
     //[test]touInt
     CPPUNIT_ASSERT_EQUAL(0, (int)addr1->touInt());
-    CPPUNIT_ASSERT_EQUAL((int)0xAC106501, (int)addr2->touInt());
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0xAC106501, addr2->touInt());
     
     //[test]toStr
     CPPUNIT_ASSERT_EQUAL(0, strcmp("0.0.0.0", addr1->toStr()));
@@ -54,6 +63,26 @@ void FIXTURE_NAME::test_ipaddress_core(){
     addr2->set(0xc0a80101);
     CPPUNIT_ASSERT_EQUAL(0, strcmp("192.168.1.1", addr2->toStr()));
     
+    //copy constructor and operators
+    copy_constructor_test(*addr2, 0xc0a80101);
+    
+    IPAddress addr_copied = *addr2;
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0xc0a80101, addr_copied.touInt());
+    
     delete addr1;
     delete addr2;
+    
 }
+
+void FIXTURE_NAME::test_ipaddress_static(){
+    char buf[32];
+    
+    //IPAddress:uitoip()
+    CPPUNIT_ASSERT(nullptr == IPAddress::uitoip(0xAC106501, buf, IP_ADDR_STR_LEN - 1));
+    CPPUNIT_ASSERT(buf == IPAddress::uitoip(0xAC106501, buf, 32));
+    CPPUNIT_ASSERT_EQUAL(0, strcmp("172.16.101.1", buf));
+    
+    //IPAddress:iptoui()
+    CPPUNIT_ASSERT_EQUAL(0xAC106501, IPAddress::iptoui((char *)"172.16.101.1"));
+}
+
