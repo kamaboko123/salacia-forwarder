771f0ec382a5380b6091a4d00bde1a132be82ddb
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-03-27 22:33:10 +0900

    change : [ci] test case name for Route.

--- forwarder/ci/src/route_table_test.cpp
+++ forwarder/ci/src/route_table_test.cpp
@@ -10,14 +10,14 @@
 
 class FIXTURE_NAME : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(FIXTURE_NAME);
-    CPPUNIT_TEST(test_route_core);
+    CPPUNIT_TEST(test_route_constructor);
     CPPUNIT_TEST_SUITE_END();
 
 public:
     void setUp();
     void tearDown();
 protected:
-    void test_route_core();
+    void test_route_constructor();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(FIXTURE_NAME);
@@ -26,9 +26,10 @@ void FIXTURE_NAME::setUp() {}
 
 void FIXTURE_NAME::tearDown() {}
 
-void FIXTURE_NAME::test_route_core(){
+void FIXTURE_NAME::test_route_constructor(){
     Route r1("10.0.0.0/8");
     
+    //str
     Array<IPAddress> bests;
     Array<RouteType> best_types;
     CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, r1.getNexthops(RTYPE_LOCAL).getSize());
@@ -45,6 +46,26 @@ void FIXTURE_NAME::test_route_core(){
     CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, r1.getRouteTypes().getSize());
     
     CPPUNIT_ASSERT_EQUAL(0, strcmp("10.0.0.0/8", r1.getNetwork().toStr()));
+    
+    
+    //IPNetwork
+    IPNetwork nw("20.0.0.0/16");
+    Route r2(nw);
+    
+    CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, r2.getNexthops(RTYPE_LOCAL).getSize());
+    
+    CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, r2.getBestNexthops(bests));
+    CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, bests.getSize());
+    
+    CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, r2.getBestNexthops().getSize());
+    CPPUNIT_ASSERT_EQUAL(RTYPE_INVALID, r2.getBestRouteType());
+    
+    CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, r2.getRouteTypes(best_types));
+    CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, best_types.getSize());
+    
+    CPPUNIT_ASSERT_EQUAL((sfwdr::size_t)0, r2.getRouteTypes().getSize());
+    
+    CPPUNIT_ASSERT_EQUAL(0, strcmp("20.0.0.0/16", r2.getNetwork().toStr()));
     
 }
 
