45e111c627e4dc34f7d629bdd8a2f5e5f9e759c2
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-09-01 14:28:11 +0900

    add PointerWrapper

--- forwarder/test/MacTable/mac_table_test.cpp
+++ forwarder/test/MacTable/mac_table_test.cpp
@@ -5,18 +5,19 @@
 int main(void){
     MacTable tbl(256);
     
-    NetIf if1("enp4s0", IFTYPE_L2_ACCESS, 1);
+    NetIf if1("11h", IFTYPE_L2_ACCESS, 1);
+    PointerWrapper<NetIf *> _if1(&if1);
+    
     MacAddress mac1("11:22:33:44:55:66");
-    tbl.update(mac1, &if1);
-    printf("%s\n", tbl.get(mac1)->getIfName());
+    tbl.update(mac1, _if1);
+    printf("%s\n", tbl.get(mac1).unwrap()->getIfName());
+    tbl.refresh();
+    
+    sleep(11);
     
     tbl.refresh();
     
-    sleep(10);
-    
-    tbl.refresh();
-    
-    if(tbl.get(mac1) == NULL){
+    if(tbl.get(mac1).unwrap() == nullptr){
         printf("[main]deleted\n");
     }
     return(0);
